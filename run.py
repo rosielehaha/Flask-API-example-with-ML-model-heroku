061520703b7d14277676e7e37e14694bb2969504
rosielehaha <rosielehaha@gmail.com>
2022-11-13 12:43:24 +0800

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app import app
 
 @app.route('/')
 def index():
-    return 'Flask API started'
+    return 'Flask API started_______'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=False)
