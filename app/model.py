713d3a48286990acbab9700699f698bb9de38009
rosielehaha <rosielehaha@gmail.com>
2022-11-13 13:11:16 +0800

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,16 +1,13 @@
 # -*- coding: UTF-8 -*-
-from keras.models import load_model
-from numpy import loadtxt
-import numpy as np
-import pandas as pd
-import time 
+import pickle
+import gzip
 
-
-
-model=load_model('app/model/pump4_ANN_0606.h5')
+# 載入Model
+with gzip.open('app/model/xgboost-iris.pgz', 'r') as f:
+    xgboostModel = pickle.load(f)
 
 
 def predict(input):
-    pred=model.predict(input)[0]
-    print("我自己的model預測結果是",pred)
+    pred=xgboostModel.predict(input)[0]
+    print(pred)
     return 
